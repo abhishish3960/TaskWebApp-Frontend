d4990444f24627707f208605d3457320484d0175
Abhishek Ashish <abhishekashish2001@gmail.com>
2024-06-28 04:12:36 +0530

    initial commit

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'https://taskwebapp.onrender.com';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/api/tasks`);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${this.apiUrl}/api/tasks`, task);
+  }
+
+  updateTask(id: number, task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/api/tasks/${id}`, task);
+  }
+
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/api/tasks/${id}`);
+  }
+
+  
+  duplicateTask(id: number): Observable<Task> {
+    return this.http.post<Task>(`${this.apiUrl}/api/tasks/duplicate/${id}`, {});
+  }
+}
